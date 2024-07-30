b381ca434c2ec406e36c378a9ccc126b6b786dae
“Zahed75” <zahedhasan.cs@gmail.com>
2024-07-30 16:50:15 +0600

    productSpecification added

--- src/modules/Products/model.js
+++ src/modules/Products/model.js
@@ -1,135 +1,3 @@
-// const { required, types } = require('joi');
-// const mongoose = require('mongoose');
-
-// const ProductSchema = new mongoose.Schema({
-//   categoryId: [{
-//     type: mongoose.Types.ObjectId,
-//     required: true,
-//     ref: 'category' 
-//   }],
-//   productName: {
-//     type: String,
-//     maxlength: 300,
-//     required: true
-//   },
-
-
-
-//   productSlug: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-
-
-//   productBrand: {
-//     type: String,
-//     required: true
-//   },
-//   productCode: {
-//     type: String,
-
-//   },
-
-//   productImage: {
-//     type: String
-//   },
-//   productGallery: [String],
-
-//   productVideos: [String],
-
-
-//   productStatus: {
-
-//     type: String,
-//     enum: ['Published', 'Draft'],
-//     required: true
-//   },
-
-
-//   date: {
-//     type: String// Automatically set the date when the product is added
-//   },
-
-//   productDescription: {
-//     type: String,
-
-//   },
-
-//   seo: {
-//     productTitle: {
-//       type: String,
-//       maxlength: 100
-//     },
-
-//     prodDescription: {
-//       type: String,
-//       maxlength: 100
-//     },
-
-//     productTags: [
-//     ],
-//     productNotes: {
-//       type: String,
-//       maxlength: 1000
-//     }
-//   },
-//   productShortDescription: {
-//     type: String,
-//     maxlength: 2000
-//   },
-//   general: {
-//     regularPrice: {
-//       type: Number,
-//       required: true
-//     },
-
-//     salePrice: Number,
-//     taxStatus: String,
-//     taxClass: String,
-
-//   },
-
-
-
-//   inventory: {
-//     sku: String,
-//     stockManagement: Boolean,
-
-//     stockStatus: {
-//       type: String,
-//       enum: ['In Stock', 'Out of Stock', 'On Backorder']
-//     },
-
-//     soldIndividually: Boolean,
-
-
-//     inventoryStatus: {
-//       type: String,
-//       enum: ['Only Online', 'Only Offline', 'Online & Offline']
-//     }
-
-//   },
-
-//   shipping: {
-//     productDimensions: {
-//       height: Number,
-//       width: Number,
-//       length: Number
-//     },
-//     weight: Number
-//   }
-// });
-
-
-
-
-// const ProductModel = mongoose.model('Product', ProductSchema);
-
-// module.exports = ProductModel;
-
-
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
@@ -173,6 +41,11 @@ const ProductSchema = new Schema({
   productDescription: {
     type: String
   },
+
+  productSpecification: {
+    type: String,
+  },
+  
   seo: {
     productTitle: {
       type: String,
