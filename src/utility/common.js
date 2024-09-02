c4fde569cf9ce3ea0c3a77864852983225786cda
“Zahed75” <zahedhasan.cs@gmail.com>
2024-09-02 10:54:38 +0600

    OTP 3 digit issues fixed

--- src/utility/common.js
+++ src/utility/common.js
@@ -1,31 +1,38 @@
 const generateOTP = () => {
-    let digits = '0123456789';
-    let OTP = '';
-    for (let i = 0; i < 4; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)];
-    }
-    return OTP;
-  };
-  
-  const asyncHandler = (func) => (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next);
-  };
-  
-  function convertTo4Digit(number) {
-    let numStr = String(number);
-  
-    let zerosNeeded = 4 - numStr.length;
-  
-    for (let i = 0; i < zerosNeeded; i++) {
-      numStr = '0' + numStr;
-    }
-  
-    return numStr;
+  let digits = '0123456789';
+  let OTP = '';
+
+  // Generate exactly 4 digits for the OTP
+  for (let i = 0; i < 4; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
   }
-  
-  module.exports = {
-    generateOTP,
-    asyncHandler,
-    convertTo4Digit,
-  };
-  
+
+  // If for any reason the OTP is not 4 digits, regenerate it
+  if (OTP.length !== 4) {
+    return generateOTP();
+  }
+
+  return OTP;
+};
+
+const asyncHandler = (func) => (req, res, next) => {
+  Promise.resolve(func(req, res, next)).catch(next);
+};
+
+function convertTo4Digit(number) {
+  let numStr = String(number);
+
+  let zerosNeeded = 4 - numStr.length;
+
+  for (let i = 0; i < zerosNeeded; i++) {
+    numStr = '0' + numStr;
+  }
+
+  return numStr;
+}
+
+module.exports = {
+  generateOTP,
+  asyncHandler,
+  convertTo4Digit,
+};
